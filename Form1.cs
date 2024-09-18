319864159b1e079ac053704e38c9b5fc325defeb
Maximo <luismaximonava@gmail.com>
2024-09-18 08:07:19 -0700

    metodo suma

--- Form1.cs
+++ Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics.CodeAnalysis;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,22 @@ namespace Max
 {
     public partial class Form1 : Form
     {
+        int resultado = 0;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int numero1 = 5; 
+            int numero2 = 8;
+            int resultado = Suma(numero1, numero2);
+        }
+        private int Suma(int x, int y)
+        {
+            resultado= x + y;
+            return resultado;
         }
     }
 }
