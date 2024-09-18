9ff138262a66417b9875896910da49b419d6d6f0
Maximo <luismaximonava@gmail.com>
2024-09-18 16:59:17 -0700

    metodo suma

--- Form1.cs
+++ Form1.cs
@@ -13,22 +13,53 @@ namespace Max
 {
     public partial class Form1 : Form
     {
-        int resultado = 0;
+
+        double resultado = 0;
         public Form1()
         {
             InitializeComponent();
         }
 
         private void Form1_Load(object sender, EventArgs e)
-        {
-            int numero1 = 5; 
-            int numero2 = 8;
-            int resultado = Suma(numero1, numero2);
+        {        
+            double numero1 = 5;
+            double numero2 = 8;
+            operaciones op=new operaciones();
+            double resultado = op.Suma(numero1, numero2);
         }
-        private int Suma(int x, int y)
-        {
-            resultado= x + y;
-            return resultado;
+        class operaciones
+        { 
+         public double Suma(double x, double y)
+            {
+
+                double resultado= x + y;
+                return resultado;
+            }
+            public double Resta(double x, double y)
+            {
+                double resultado = x - y;
+                return resultado;
+            }
+            public double Multp(double x, double y)
+            {
+                double num1=2;
+                double num2 = 2;
+                double resultado = x * y;
+                do
+                {
+                    Console.Write(resultado);
+                    resultado++;
+                }
+                while (num1 != 0 || num2 !=0);
+                return resultado;
+            }
+            public int Div(int x, int y)
+            {
+                int resultado = x / y;
+                return resultado;
+            }
         }
+
+      
     }
 }
