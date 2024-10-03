001464068c1138f2892020af8191abc63da36ec5
Maximo <luismaximonava@gmail.com>
2024-10-02 21:16:49 -0700

    Calificaciones

---
+++ Calificaciones/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calificaciones
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void VerMateria(object sender, EventArgs e) 
+        {
+            Materias _materias = new Materias();
+            _materias.Show();
+        }
+        private void VerAlumno(object sender, EventArgs e)
+        { 
+            Alumno _alumno = new Alumno();
+            _alumno.Show(); 
+        }
+
+ 
+    }
+}
---
+++ Calificaciones/Materias.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calificaciones
+{
+    public partial class Materias : Form
+    {
+        public Materias()
+        {
+            InitializeComponent();
+        }
+
+        private void TipoDeMateria(object sender, EventArgs e)
+        {
+            TipoDeMateria _tipomat = new TipoDeMateria();
+            _tipomat.Show();
+        }
+
+        private void Materias_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
